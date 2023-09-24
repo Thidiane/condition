e376fe5a367dcd21bdaef4aa2a5562d4036aab12
Thidiane <145430867+Thidiane@users.noreply.github.com>
2023-09-24 18:46:24 +0100

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let a= 4;
+let y= 4;
+let z= 3;
+
+switch(a){
+    case y:
+        console.log("égale à y");
+        break;
+    case z:
+        console.log("égale à z");
+        break;
+    default:
+        console.log("égale à rien");
+}       
